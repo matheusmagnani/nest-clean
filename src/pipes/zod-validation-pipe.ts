958238c7cf92ecf96fc0f033006de2dd42aa6487
matheusmagnani <mrmatheusmagnani@gmail.com>
2025-02-03 17:07:29 -0300

    feat: now we have a user account creation route

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,22 @@
+import {
+  PipeTransform,
+  BadRequestException
+} from '@nestjs/common'
+import { ZodError, ZodObject } from 'zod'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodObject<any>) {}
+
+  transform(value: unknown){
+    try {
+      this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError){
+        throw new BadRequestException({errors: error.format(), message: 'Validation failed', statusCode: 400})
+      }
+
+      throw new BadRequestException('Validation failed')
+    }
+    return value
+  }
+}
