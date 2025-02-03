08c95cc36e2b4dd7c07afc453651e0b9f123610e
matheusmagnani <mrmatheusmagnani@gmail.com>
2025-02-03 19:27:59 -0300

    feat: now we have an authentication route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controller/create-account.controller';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env';
+import { AuthModule } from './auth/auth.module';
+import { AuthenticateController } from './controller/authenticate.controller';
 
 @Module({
-  imports: [],
-  controllers: [CreateAccountController],
+  imports: [ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true
+    }),
+    AuthModule,
+  ],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
